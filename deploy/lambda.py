4bffdf40c6370a19279a5c26648ebd4d2b20e227
Bingbing Liu <50776512+bingbingliu18@users.noreply.github.com>
2023-04-04 10:48:39 +0800

    Update lambda.py

--- deploy/lambda.py
+++ deploy/lambda.py
@@ -1,8 +1,8 @@
 import boto3
 import json
-
+description_list=['Bug Fixes']
+action_list=['db-upgrade']
 notification = "This is notification for Aurora and DocumentDB patch \n"
-
 def query_docdb():
     global notification
     EC2 = boto3.client('ec2')
@@ -14,26 +14,37 @@ def query_docdb():
         if len(pma['PendingMaintenanceActions']) > 0:
             num = len(pma['PendingMaintenanceActions'])
             for inst in pma['PendingMaintenanceActions']:
-                mw_type=inst['PendingMaintenanceActionDetails'][0]['Description']
-                if mw_type != 'Bug Fixes':
+                mw_description=inst['PendingMaintenanceActionDetails'][0]['Description']
+                mw_action=inst['PendingMaintenanceActionDetails'][0]['Action']
+                if mw_description not in description_list and mw_action not in action_list:
                     num = num - 1
                     continue
+                temp_string = "**********************************************" +  "\n"
+                notification += temp_string
                 temp_string = "Region: " + REGION['RegionName'] +  " has Aurora or docDB cluster needed to be upgrade.\n"
                 notification += temp_string
-                temp_string = "The Cluster has pending maintenance action: " + inst['ResourceIdentifier'] + "\n"
+                temp_string = "Mw_description: " + mw_description +  "\n"
+                notification += temp_string
+                temp_string = "Mw_action: " + mw_action +  "\n"
+                notification += temp_string
+                temp_string = "The Resource has pending maintenance action: " + inst['ResourceIdentifier'] + "\n"
                 notification += temp_string
                 cls_identifier=inst['ResourceIdentifier'].split(':')[-1]
                 try:
                     cls = docDB.describe_db_clusters(DBClusterIdentifier=cls_identifier)
                 except Exception as e:
-                        temp_string = "Failed to get cluster information " + cls_identifier + " Exception: " + e + "\n"
+                        temp_string = "Failed to get cluster information! This is instance level notification" + "\n" 
+                        notification += temp_string
+                        temp_string = "**********************************************" +  "\n"
                         notification += temp_string
                         continue
                 dbclus=cls['DBClusters']
                 mw = dbclus[0]['PreferredMaintenanceWindow']
                 temp_string = "The Cluster maintenance window UTC time: " + mw + "\n"
                 notification += temp_string
-            temp_string = "Region: " + REGION['RegionName'] + " has total " + str(num) + " cluster found.\n"   
+            temp_string = "Region: " + REGION['RegionName'] + " has total " + str(num) + " resource found.\n"   
+            notification += temp_string
+            temp_string = "**********************************************" +  "\n"
             notification += temp_string
         else:
             temp_string = "Region: " + REGION['RegionName'] + " No Cluster found.\n"
@@ -44,7 +55,7 @@ def lambda_handler(event, context):
     query_docdb()
     print (notification)
     sns_response = sns_client.publish (
-        TargetArn = "arn:aws:sns:us-east-1:02818****:docdb-patch-notification",
+        TargetArn = "arn:aws:sns:us-east-1:028183925784:docdb-patch-notification",
         Subject = "DocumentDB Patch Notification",
         Message = json.dumps({'default': notification}),
         MessageStructure = 'json'
